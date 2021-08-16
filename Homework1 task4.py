6dc3649ffc85e8a3ee79c70890a0448587327d59
geekevgin <evgenia.belousova@gmail.com>
2021-08-16 14:15:01 +0300

    Initial commit

--- Homework1 task4.py
+++ Homework1 task4.py
@@ -2,12 +2,15 @@ n = int(input('Введите число:'))
 num_of_it = 0
 div = 1
 max_digit = 0
-has_more_digits = 1
-while num_of_it < 4:
+has_more_digits = 0
+while num_of_it < 5:
     num_of_it += 1
     digit = (n // div) % 10
     div = div * 10
+    has_more_digits = div * 10
     if max_digit < digit:
         max_digit = digit
+    if has_more_digits != 0:
+        has_more_digits = digit
 
 print(max_digit)
